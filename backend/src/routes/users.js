d61f9acb87229ec6490683be90ddf02d0a404572
Pcjmy <1716045191@qq.com>
2023-03-31 11:51:01 +0800

    feat: 开发接口

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,4 +1,5 @@
 const router = require('koa-router')()
+const { register } = require('../controller/user')
 
 router.prefix('/users')
 
@@ -8,6 +9,28 @@ router.get('/', function (ctx, next) {
 
 router.get('/bar', function (ctx, next) {
   ctx.body = 'this is a users/bar response'
+})
+
+// 注册
+router.post('/register', async (ctx, next) => {
+  // 获取注册信息
+  const userInfo = ctx.request.body
+  // 提交注册
+  try {
+    const newUser = await register(userInfo) // 调用 controller
+    // 成功
+    ctx.body = {
+      errno: 0,
+      data: newUser
+    }
+  } catch (ex) {
+    // 失败
+    console.error('注册失败')
+    ctx.body = {
+      errno: -1,
+      message: '注册失败'
+    }
+  }
 })
 
 module.exports = router
